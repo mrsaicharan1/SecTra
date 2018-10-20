38e13242f55d4ad9981ce23c2ebb45ad46afefcf
mrsaicharan1 <saicharan.reddy1@gmail.com>
2018-10-20 10:12:40 +0530

    Added imdonwloader scripts+training data

---
+++ image_downloader.py
@@ -0,0 +1,7 @@
+from google_images_download import google_images_download
+
+response = google_images_download.googleimagesdownload()
+
+arguments = {"keywords":"Cigarettes,power bank,blades","limit":100,"print_urls":True,'px':'172.31.1.4:8080'}   #creating list of arguments
+
+paths = response.download(arguments)
