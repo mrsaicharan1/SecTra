2cea0de21a8687f53d692a0e2d9b72df6dc5ac30
Sashank Mishra <sashankmishra27@gmail.com>
2018-10-20 22:27:50 +0530

    added basic app (#3)
    * Basic app added(2)
    
    * Basic app added(2)

---
+++ SectraApp/app/src/main/java/org/freeflo/sashank/sectra/fragments/DetailsFragment.kt
@@ -0,0 +1,18 @@
+package org.freeflo.sashank.sectra.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.freeflo.sashank.sectra.R
+
+class DetailsFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
