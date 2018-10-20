cbd116c76ed23869950b15eba67be7ade885c204
shreyanshdwivedi <shreyanshdwivedi1997@gmail.com>
2018-10-21 01:50:57 +0530

    RestAPI created to upload image

--- object-detection/webApp/objectDetection/urls.py
+++ object-detection/webApp/objectDetection/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
+from .views import FileView
 
 app_name = 'app'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^upload/$', FileView.as_view(), name='file-upload'),
 ]
--- object-detection/webApp/objectDetection/views.py
+++ object-detection/webApp/objectDetection/views.py
@@ -7,7 +7,25 @@ from django.db.models import Q
 import subprocess as s
 import os
 
+from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status	
+
+from .serializers import FileSerializer
+
 
 def index(request):
 	s.call('python3 ../../models-master/research/object_detection/object_detection_tutorial.py', shell=True)
 	return HttpResponse("OK")
+
+
+class FileView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+    def post(self, request, *args, **kwargs):
+    	file_serializer = FileSerializer(data=request.data)
+    	if file_serializer.is_valid():
+    		file_serializer.save()
+    		return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+    	else:
+    		return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
