8d2825dea4f334a358ac162dbdded940a88e6282
mrsaicharan1 <saicharan.reddy1@gmail.com>
2018-10-20 20:06:18 +0530

    Added model & prediction

--- image_downloader.py
+++ image_downloader.py
@@ -2,6 +2,7 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
-arguments = {"keywords":"Cigarettes,power bank,blades","limit":100,"print_urls":True,'px':'172.31.1.4:8080'}   #creating list of arguments
+arguments = {"keywords":"Cigarette,power-bank,sharp blade","limit":500,"print_urls":True,'px':'172.31.1.4:8080'}   #creating list of arguments
 
 paths = response.download(arguments)
+print(paths)
---
+++ predict.py
@@ -0,0 +1,13 @@
+import os
+from imageai.Prediction.Custom import CustomImagePrediction
+
+execution_path = os.getcwd()
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath(os.path.join(execution_path,'model_ex-077_acc-0.781250.h5'))
+prediction.setJsonPath(os.path.join(execution_path, "model_class.json"))
+prediction.loadModel(num_objects=2)
+
+predictions, probabilities = prediction.predictImage(os.path.join(execution_path, "4.jpg"), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction + " : " + eachProbability)
