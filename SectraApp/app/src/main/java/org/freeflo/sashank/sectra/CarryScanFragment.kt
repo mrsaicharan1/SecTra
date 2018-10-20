9b87ce6c12175e86c3eeab206c0441b5db56e81b
sashank27 <sashankmishra27@gmail.com>
2018-10-20 12:53:47 +0530

    Basic app added

---
+++ SectraApp/app/src/main/java/org/freeflo/sashank/sectra/CarryScanFragment.kt
@@ -0,0 +1,24 @@
+package org.freeflo.sashank.sectra
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_carry.*
+
+class CarryScanFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_carry, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        cameraView.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        cameraView.stop()
+    }
+}
