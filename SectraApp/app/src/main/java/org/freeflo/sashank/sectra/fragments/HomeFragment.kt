f27aabdf5e59cb3187b47d24eb2e84150a336631
sashank27 <sashankmishra27@gmail.com>
2018-10-20 19:57:38 +0530

    Basic app added(2)

---
+++ SectraApp/app/src/main/java/org/freeflo/sashank/sectra/fragments/HomeFragment.kt
@@ -0,0 +1,32 @@
+package org.freeflo.sashank.sectra.fragments
+
+import android.app.ProgressDialog
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import kotlinx.android.synthetic.main.fragment_home.*
+import org.freeflo.sashank.sectra.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        checkInfoBT.setOnClickListener {
+            val pnr = pnrET.text
+            val dialog = ProgressDialog.show(activity, "Fetching Details", "Loading...")
+            Han
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.frame, DetailsFragment())?.commit()
+            dialog.dismiss()
+        }
+
+    }
+}
