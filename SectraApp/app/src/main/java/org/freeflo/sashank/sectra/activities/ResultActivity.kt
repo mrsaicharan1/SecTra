9fd932198565ff7f37e7a98c74b947d8b42e279a
sashank27 <sashankmishra27@gmail.com>
2018-10-21 09:32:27 +0530

    UI done

--- SectraApp/app/src/main/java/org/freeflo/sashank/sectra/activities/ResultActivity.kt
+++ SectraApp/app/src/main/java/org/freeflo/sashank/sectra/activities/ResultActivity.kt
@@ -2,6 +2,10 @@ package org.freeflo.sashank.sectra.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.View
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.app_bar.*
 import org.freeflo.sashank.sectra.R
 
 class ResultActivity : AppCompatActivity() {
@@ -9,6 +13,14 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        appBarTitle.text = "Your Items"
+        menuIV.visibility = View.GONE
+
+
     }
 }
